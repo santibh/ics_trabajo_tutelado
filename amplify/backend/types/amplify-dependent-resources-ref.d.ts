e7a7fd49a17862e912240b6bf96e6b11065b3510
santibh <santiagobarreirohermida@gmail.com>
2021-12-17 22:06:15 +0100

    Añadida autenticación

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "icstrabajotuteladobfc4bfbc": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
