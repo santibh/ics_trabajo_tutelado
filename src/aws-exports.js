45abbe3a72828e84a7df0589164100711d12851a
santibh <santiagobarreirohermida@gmail.com>
2021-12-18 12:00:57 +0100

    Añadida la API de GraphQL

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,11 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://nkozkxbo3ncgbj6p4uiv4djpie.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_appsync_apiKey": "da2-efbeusgdqzeufjibaisqtrmzka"
 };
 
 
